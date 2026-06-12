ddce84b8a0db9dd2a7700fef92a2ab5270aa9927
koneti Praneeth Kumar <168185405+konetipraneethkumar@users.noreply.github.com>
2026-06-12 10:16:27 +0530

    Time: 3 ms (51.27%) | Memory: 12.3 MB (96.37%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
@@ -4,7 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        even_dig_count = 0
+        #approach 1
+        """
+         even_dig_count = 0
         for i in range(len(nums)):
             dig_count = 0
             num = nums[i]
@@ -14,6 +16,25 @@ class Solution(object):
             if dig_count %2 ==0:
                 even_dig_count +=1
         return even_dig_count
+        """
+        #approach 2
+        """
+        even_dig_count = 0
+        for i in range(len(nums)):
+            dig_count = 0
+            while nums[i] != 0:
+                nums[i] = nums[i] // 10
+                dig_count+=1
+            if dig_count %2 ==0:
+                even_dig_count +=1
+        return even_dig_count"""
+        #approach 3
+        count = 0
+        for i in nums:
+            digits = len(str(i))
+            if digits %2 ==0:
+                count+=1
+        return count
 sol = Solution()
 nums = [555,901,482,1771]
 print(sol.findNumbers(nums))
