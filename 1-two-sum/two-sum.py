bb17dca365ad161f0f66b630d7698af0a7d66498
koneti Praneeth Kumar <168185405+konetipraneethkumar@users.noreply.github.com>
2026-08-14 17:05:31 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (53.08%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}
+        for i in range(len(nums)):
+            dic[nums[i]]  = i
+        for i in range(len(nums)):
+            if target - nums[i] in dic and dic[target - nums[i]] != i:
+                return [i,dic[target - nums[i]]]
+        return None
+                
+
+        
