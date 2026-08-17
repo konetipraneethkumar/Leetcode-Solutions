6a89d4e3e9035a625e103b393dc59fcff39ce9c8
koneti Praneeth Kumar <168185405+konetipraneethkumar@users.noreply.github.com>
2026-08-17 20:13:56 +0530

    Time: 3 ms (30.90%) | Memory: 13.1 MB (19.97%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        largest = -float('inf')
+        second_largest = -float('inf')
+        third_largest = -float('inf')
+        for i in range(len(nums)):
+            if nums[i] == largest or nums[i] == second_largest or nums[i] == third_largest:
+                continue
+
+            if nums[i] > largest:
+                third_largest = second_largest
+                second_largest = largest
+                largest = nums[i]
+            elif nums[i]<largest and nums[i]>second_largest:
+                third_largest = second_largest
+                second_largest = nums[i]
+            elif nums[i]<largest and nums[i]<second_largest and nums[i]> third_largest:
+                third_largest = nums[i]
+
+        if third_largest != -float('inf'):
+            return third_largest
+        else:
+            return largest
+
+        
