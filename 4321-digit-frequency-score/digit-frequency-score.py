b7fb79640f9e925895438211c25c3a356f1cb05b
koneti Praneeth Kumar <168185405+konetipraneethkumar@users.noreply.github.com>
2026-08-22 18:37:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (57.88%) - LeetSync

--- 4321-digit-frequency-score/digit-frequency-score.py
+++ 4321-digit-frequency-score/digit-frequency-score.py
@@ -1,15 +1,16 @@
 class Solution:
     def digitFrequencyScore(self, n: int) -> int:
-        h_map = {}
+        #h_map = {}
+        total = 0
         while n >0:
-            dig = n%10
-            if dig not in h_map:
-                h_map[dig] = 1
-            else:
-                h_map[dig] +=1
+            total += n%10
+            #if dig not in h_map:
+            #    h_map[dig] = 1
+            #else:
+                #h_map[dig] +=1
             n = n // 10
-        sum = 0
-        for key, val in h_map.items():
-            sum = sum + (key * val)
-        return sum
+        #sum = 0
+        #for key, val in h_map.items():
+        #    sum = sum + (key * val)
+        return total
         
