b53f0459af9706094032b8bb673d47e50395beea
koneti Praneeth Kumar <168185405+konetipraneethkumar@users.noreply.github.com>
2026-08-25 23:21:01 +0530

    Time: 2 ms (56.32%) | Memory: 20.6 MB (7.00%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,17 +1,27 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        dic = {}
-        for i in range(len(nums)):
-            dic[nums[i]]  = i
-        for i in range(len(nums)):
-            if target - nums[i] in dic and dic[target - nums[i]] != i:
-                return [i,dic[target - nums[i]]]
-        return None
-                
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+      #left = 0
+      #right = len(nums) - 1
+      #
+      #while left < right:
+      #    sum = nums[left] + nums[right]
 
+      #    if sum == target:
+      #        return [left,right]
+
+      #    elif sum > target:
+      #        right -=1
+
+      #    elif sum < target:
+      #        left +=1
+      #return None # This approach will work for only sorted array 
+      
+
+      # Using Hash Table
+        hash_table = {}
+        for i in range(len(nums)):
+            hash_table[nums[i]] = i
+        for i in range(len(nums)):
+            if target - nums[i] in hash_table and hash_table[target - nums[i]] != i:
+                return [i,hash_table[target - nums[i]]]
         
