500fc9f128be9a4b3abf72123fa245f797d755a2
koneti Praneeth Kumar <168185405+konetipraneethkumar@users.noreply.github.com>
2026-09-04 11:57:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (58.13%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,17 +1,34 @@
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
         """
-        Do not return anything, modify nums1 in-place instead.
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        j = 0
-        for i in range(m,len(nums1)):
-            if j < n:
-                nums1[i] = nums2[j]
-                j+=1
-        nums1.sort()
+        res= []
+        n1 = 0
+        n2 = 0
+        while n1 < m and n2 < n:
+            if nums1[n1] <= nums2[n2]:
+                res.append(nums1[n1])
+                n1 +=1
+            else:
+                res.append(nums2[n2])
+                n2 +=1
+        while n1 < m:
+            res.append(nums1[n1])
+            n1+=1
+        while n2 < n:
+            res.append(nums2[n2])
+            n2+=1
+        nums1[:] = res
+        return 
+        
+    
 
-
-            
+        
 
 
         
